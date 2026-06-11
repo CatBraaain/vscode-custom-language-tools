705545f336fa07242a1c3fea4eeb85d900c82f69
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-11 23:15:33 +0900

    perf: restart only when target config changed

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,11 @@ export async function activate(context: vscode.ExtensionContext) {
   registrations = await registerCustomLanguageConfig();
 
   context.subscriptions.push(
-    vscode.workspace.onDidChangeConfiguration(async () => {
-      registrations.forEach((e) => e.dispose());
-      registrations = await registerCustomLanguageConfig();
+    vscode.workspace.onDidChangeConfiguration(async (event) => {
+      if (event.affectsConfiguration("customLanguageConfig.rules")) {
+        registrations.forEach((e) => e.dispose());
+        registrations = await registerCustomLanguageConfig();
+      }
     }),
     vscode.commands.registerCommand("customLanguageConfig.restartAll", async () => {
       registrations.forEach((e) => e.dispose());
