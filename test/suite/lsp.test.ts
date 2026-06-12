6e7cde8e853269a49dace9c30af947bd053b4737
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-12 14:13:39 +0900

    test: add test for lsp

---
+++ test/suite/lsp.test.ts
@@ -0,0 +1,15 @@
+import * as assert from "node:assert";
+
+import { describe, test } from "mocha";
+
+import { registerLsp } from "../../src/lsp";
+
+describe("registerLsp", () => {
+  test("register and dispose does work", async () => {
+    const lsp = await registerLsp("javascript", "echo hello");
+    assert.ok(lsp);
+    assert.doesNotThrow(() => {
+      lsp.dispose();
+    });
+  });
+});
