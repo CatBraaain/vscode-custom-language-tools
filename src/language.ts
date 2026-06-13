a9396cce9a48712da818c343590293a3475d3bf0
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-13 21:49:11 +0900

    style: vp check --fix

--- src/language.ts
+++ src/language.ts
@@ -7,15 +7,15 @@ export async function registerLangConfig(
   langs: string[],
   langConfig: LangConfig,
 ): Promise<vscode.Disposable> {
-  Logger.info(`Property registering\n${JSON.stringify({langs,langConfig})}`);
+  Logger.info(`Property registering\n${JSON.stringify({ langs, langConfig })}`);
   const disposables = langs.map((lang) =>
     vscode.languages.setLanguageConfiguration(lang, langConfig),
   );
-  Logger.info(`Property registered\n${JSON.stringify({langs,langConfig})}`);
+  Logger.info(`Property registered\n${JSON.stringify({ langs, langConfig })}`);
 
   return new vscode.Disposable(() => {
-    Logger.info(`Property unregistering\n${JSON.stringify({langs,langConfig})}`);
+    Logger.info(`Property unregistering\n${JSON.stringify({ langs, langConfig })}`);
     disposables.forEach((d) => d.dispose());
-    Logger.info(`Property unregistered\n${JSON.stringify({langs,langConfig})}`);
+    Logger.info(`Property unregistered\n${JSON.stringify({ langs, langConfig })}`);
   });
 }
--- src/lsp.ts
+++ src/lsp.ts
@@ -52,13 +52,13 @@ export async function registerLsp(langs: string[], command: string): Promise<vsc
     }
   });
 
-  Logger.info(`LSP registering\n${JSON.stringify({langs,command})}`);
+  Logger.info(`LSP registering\n${JSON.stringify({ langs, command })}`);
   await client.start();
-  Logger.info(`LSP registered\n${JSON.stringify({langs,command})}`);
+  Logger.info(`LSP registered\n${JSON.stringify({ langs, command })}`);
 
   return new vscode.Disposable(async () => {
-    Logger.info(`LSP unregistering\n${JSON.stringify({langs,command})}`);
+    Logger.info(`LSP unregistering\n${JSON.stringify({ langs, command })}`);
     await client.stop();
-    Logger.info(`LSP unregistered\n${JSON.stringify({langs,command})}`);
+    Logger.info(`LSP unregistered\n${JSON.stringify({ langs, command })}`);
   });
 }
