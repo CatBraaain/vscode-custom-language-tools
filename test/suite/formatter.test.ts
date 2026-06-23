1e4ada9a0f8d14a89179ae8d44fc88eabe6f6c40
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-23 16:34:04 +0900

    test: fix oudated test

--- test/suite/formatter.test.ts
+++ test/suite/formatter.test.ts
@@ -3,10 +3,12 @@ import * as assert from "node:assert";
 import { describe, test } from "mocha";
 
 import { registerFormatter } from "../../src/formatter";
+import { ToolManager } from "../../src/tool-manager";
 
 describe("registerFormatter", () => {
   test("register and dispose does work", () => {
-    const formatter = registerFormatter(["javascript"], "prettier --write", "Prettier");
+    const toolManager = new ToolManager();
+    const formatter = registerFormatter(["javascript"], toolManager);
     assert.ok(formatter);
     assert.doesNotThrow(() => {
       formatter.dispose();
