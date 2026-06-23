3b02da5346a82a401dac5cf9a0577d2c9d6abb56
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-24 00:50:09 +0900

    refactor: improve readability

--- src/formatter.ts
+++ src/formatter.ts
@@ -40,7 +40,8 @@ function registerFullFormatter(
       options: vscode.FormattingOptions,
       token: vscode.CancellationToken,
     ): vscode.ProviderResult<vscode.TextEdit[]> {
-      return formatDocument(toolManager, document, undefined, options, token);
+      const formatter = new DocumentFormatter(toolManager, document, options, token);
+      return formatter.format();
     },
   } satisfies vscode.DocumentFormattingEditProvider);
 }
@@ -56,129 +57,128 @@ function registerRangeFormatter(
       options: vscode.FormattingOptions,
       token: vscode.CancellationToken,
     ): vscode.ProviderResult<vscode.TextEdit[]> {
-      return formatDocument(toolManager, document, range, options, token);
+      const formatter = new DocumentFormatter(toolManager, document, options, token);
+      return formatter.format(range);
     },
   } satisfies vscode.DocumentRangeFormattingEditProvider);
 }
 
-async function formatDocument(
-  toolManager: ToolManager,
-  document: vscode.TextDocument,
-  range: vscode.Range | undefined,
-  options: vscode.FormattingOptions,
-  token: vscode.CancellationToken,
-): Promise<vscode.TextEdit[]> {
-  Logger.info("Format...");
-  const editor = vscode.window.visibleTextEditors.find((editor) => editor.document === document);
-  if (editor) {
-    const lspContexts = toolManager.ruleContexts.filter(
-      (ctx) => ctx.lspClients && vscode.languages.match(ctx.rule.document, document),
+export class DocumentFormatter {
+  constructor(
+    private toolManager: ToolManager,
+    private document: vscode.TextDocument,
+    private options: vscode.FormattingOptions,
+    private token: vscode.CancellationToken,
+  ) {}
+
+  public async format(range?: vscode.Range): Promise<vscode.TextEdit[]> {
+    Logger.info("Format...");
+    const editor = vscode.window.visibleTextEditors.find(
+      (editor) => editor.document === this.document,
     );
-    for (const ctx of lspContexts) {
-      for (const client of ctx.lspClients!) {
-        const edits = await formatWithLsp(client, document, options, range, token);
-        await editor.edit((builder) => {
-          for (const edit of edits) {
-            builder.replace(edit.range, edit.newText);
-          }
-        });
+    if (editor) {
+      const lspContexts = this.toolManager.ruleContexts.filter(
+        (ctx) => ctx.lspClients && vscode.languages.match(ctx.rule.document, this.document),
+      );
+      for (const ctx of lspContexts) {
+        for (const client of ctx.lspClients!) {
+          const edits = await this.formatWithLsp(client, range);
+          await editor.edit((builder) => {
+            for (const edit of edits) {
+              builder.replace(edit.range, edit.newText);
+            }
+          });
+        }
       }
     }
+
+    const targetRange =
+      range ?? this.document.validateRange(new vscode.Range(0, 0, this.document.lineCount, 0));
+    const newText = await this.formatWithCommand(range);
+    Logger.info("Format finished");
+    return [vscode.TextEdit.replace(targetRange, newText)];
   }
 
-  const targetRange =
-    range ?? document.validateRange(new vscode.Range(0, 0, document.lineCount, 0));
-  const newText = await formatWithCommands(toolManager, document, document.getText(range));
-  Logger.info("Format finished");
-  return [vscode.TextEdit.replace(targetRange, newText)];
-}
+  private async formatWithLsp(
+    client: LanguageClient,
+    range?: vscode.Range,
+  ): Promise<vscode.TextEdit[]> {
+    Logger.info("Requesting LSP formatting...");
 
-async function formatWithLsp(
-  client: LanguageClient,
-  document: vscode.TextDocument,
-  options: vscode.FormattingOptions,
-  range: vscode.Range | undefined,
-  token: vscode.CancellationToken,
-): Promise<vscode.TextEdit[]> {
-  Logger.info("Requesting LSP formatting...");
-
-  const response = await client.sendRequest<vscode.TextEdit[] | null>(
-    range ? DocumentRangeFormattingRequest.method : DocumentFormattingRequest.method,
-    {
-      textDocument: { uri: document.uri.toString() },
-      range,
-      options: {
-        tabSize: options.tabSize,
-        insertSpaces: options.insertSpaces,
-      },
-    } satisfies DocumentFormattingParams | DocumentRangeFormattingParams,
-    token,
-  );
-
-  if (!response) {
-    Logger.warn("LSP returned no formatting edits");
-    return [];
-  }
-
-  Logger.debug(`LSP returned ${response.length} edit(s)`);
-  return response.map(
-    (edit) =>
-      new vscode.TextEdit(
-        new vscode.Range(
-          new vscode.Position(edit.range.start.line, edit.range.start.character),
-          new vscode.Position(edit.range.end.line, edit.range.end.character),
+    const response = await client.sendRequest<vscode.TextEdit[] | null>(
+      range ? DocumentRangeFormattingRequest.method : DocumentFormattingRequest.method,
+      {
+        textDocument: { uri: this.document.uri.toString() },
+        range,
+        options: {
+          tabSize: this.options.tabSize,
+          insertSpaces: this.options.insertSpaces,
+        },
+      } satisfies DocumentFormattingParams | DocumentRangeFormattingParams,
+      this.token,
+    );
+    const edits = (response ?? []).map(
+      (edit) =>
+        new vscode.TextEdit(
+          new vscode.Range(
+            new vscode.Position(edit.range.start.line, edit.range.start.character),
+            new vscode.Position(edit.range.end.line, edit.range.end.character),
+          ),
+          edit.newText,
         ),
-        edit.newText,
-      ),
-  );
-}
+    );
 
-async function formatWithCommands(
-  toolManager: ToolManager,
-  document: vscode.TextDocument,
-  currentText: string,
-): Promise<string> {
-  const matchedRules = toolManager.ruleContexts
-    .filter(
-      (ctx) =>
-        ctx.conditionMatched &&
-        ctx.rule.action.formatter &&
-        vscode.languages.match(ctx.rule.document, document),
-    )
-    .map((ctx) => ctx.rule);
-
-  Logger.debug(`Matched formatter rules: [${matchedRules.map((r) => r.name).join(", ")}]`);
-
-  const commands = matchedRules.flatMap((rule) =>
-    rule.action.formatter!.map((formatter) => formatter.replace("${filePath}", document.fileName)),
-  );
-  // when edit files outside of workspace, use the first workspace
-  const cwd =
-    vscode.workspace.getWorkspaceFolder(document.uri)?.uri.fsPath ??
-    vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
-
-  Logger.info("Format");
-  Logger.info(`  commands: [${commands.join(", ")}]`);
-  Logger.info(`  cwd: ${cwd}`);
-
-  for (const command of commands) {
-    const res = await execa({
-      input: currentText,
-      shell: true,
-      cwd,
-      reject: false,
-      stripFinalNewline: false,
-    })`${command}`;
-
-    if (res.exitCode === 0) {
-      currentText = res.stdout;
-    } else {
-      Logger.warn(
-        `Formatter failed (${command}): ${JSON.stringify({ exitCode: res.exitCode, stderr: res.stderr })}`,
-      );
-      break;
-    }
+    Logger.debug(`LSP returned ${edits.length} edit(s)`);
+    return edits;
   }
 
-  return currentText;
+  private async formatWithCommand(range?: vscode.Range): Promise<string> {
+    const matchedRules = this.toolManager.ruleContexts
+      .filter(
+        (ctx) =>
+          ctx.conditionMatched &&
+          ctx.rule.action.formatter &&
+          vscode.languages.match(ctx.rule.document, this.document),
+      )
+      .map((ctx) => ctx.rule);
+
+    Logger.debug(`Matched formatter rules: [${matchedRules.map((r) => r.name).join(", ")}]`);
+
+    const commands = matchedRules.flatMap((rule) =>
+      rule.action.formatter!.map((formatter) =>
+        formatter.replace("${filePath}", this.document.fileName),
+      ),
+    );
+    // when edit files outside of workspace, use the first workspace
+    const cwd =
+      vscode.workspace.getWorkspaceFolder(this.document.uri)?.uri.fsPath ??
+      vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
+
+    Logger.info("Format");
+    Logger.info(`  commands: [${commands.join(", ")}]`);
+    Logger.info(`  cwd: ${cwd}`);
+
+    let currentText = this.document.getText(range);
+
+    for (const command of commands) {
+      const res = await execa({
+        input: currentText,
+        shell: true,
+        cwd,
+        reject: false,
+        stripFinalNewline: false,
+      })`${command}`;
+
+      if (res.exitCode === 0) {
+        currentText = res.stdout;
+      } else {
+        Logger.warn(
+          `Formatter failed (${command}): ${JSON.stringify({ exitCode: res.exitCode, stderr: res.stderr })}`,
+        );
+        break;
+      }
+    }
+
+    return currentText;
+  }
 }
